2d4725409431ea50eaff60e6b56816701de26b7d
wysohn <wysohn2002@naver.com>
2019-03-17 15:00:15 -0700

    Added DramaMovieKey

---
+++ Assignment4-TeamSMG/drama_movie_key.cpp
@@ -0,0 +1,34 @@
+#include "drama_movie_key.h"
+
+DramaMovieKey::DramaMovieKey(std::string title, int year, std::string director)
+	: MovieKey(title, year, director)
+{
+}
+
+int DramaMovieKey::compareTo(const Comparable * other)
+{
+	if (const DramaMovieKey* key = dynamic_cast<const DramaMovieKey*>(other)) {
+		//compare director
+		if (this->director < key->director) {
+			return -1;
+		}
+		else if (this->director > key->director) {
+			return 1;
+		}
+		//if director is same, compare title
+		else {
+			if (this->title < key->title) {
+				return -1;
+			}
+			else if(this->title > key->title){
+				return 1;
+			}
+			else {
+				return 0;
+			}
+		}
+	}
+	else {
+		throw;
+	}
+}
---
+++ Assignment4-TeamSMG/drama_movie_key.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "movie_key.h"
+
+class DramaMovieKey : public MovieKey
+{
+public:
+	DramaMovieKey(std::string title, int year, std::string director);
+
+	/*
+	-dramas (¡®D¡¯) are sorted by Director, then Title 
+	*/
+	int compareTo(const Comparable* other);
+};
+
