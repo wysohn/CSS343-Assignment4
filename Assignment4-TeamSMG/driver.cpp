58c59e1deef9211a54aac69046f6725ebb1b98df
wysohn <wysohn2002@naver.com>
2019-03-16 21:32:13 -0700

    tests for rehashing

--- Assignment4-TeamSMG/driver.cpp
+++ Assignment4-TeamSMG/driver.cpp
@@ -101,93 +101,109 @@ int main() {
 	ASSERT_EQ(32, map.get_max_size());
 
 	TempHashable temp1(50);
-	const Map<int>::Hashable* ptemp1 = &temp1;
 	int temp1_val = 50;
 
 	TempHashable temp2(53);
-	const Map<int>::Hashable* ptemp2 = &temp2;
 	int temp2_val = 53;
 
 	TempHashable temp1_collision(18);
-	const Map<int>::Hashable* ptemp1_collision = &temp1_collision;
 	int temp1_collision_val = 18;
 
 	TempHashable temp1_collision2(82);
-	const Map<int>::Hashable* ptemp1_collision2 = &temp1_collision2;
 	int temp1_collision_val2 = 82;
 
 	TempHashable temp1_collision3(114);
-	const Map<int>::Hashable* ptemp1_collision3 = &temp1_collision3;
 	int temp1_collision_val3 = 114;
 
 	//functional tests of methods (find_pair put get containsKey remove)
 	
 	//find_pair (not found)
-	Map<int>::Pair* pair = map.find_pair(ptemp1);
+	Map<int>::Pair* pair = map.find_pair(&temp1);
 	ASSERT_NULL(pair);
 
 	//put
-	int* prev = map.put(ptemp1, &temp1_val);
+	int* prev = map.put(&temp1, &temp1_val);
 	ASSERT_NULL(prev);
 	ASSERT_EQ(1, map.get_capacity());
 
 	//put (replacement test)
-	prev = map.put(ptemp1, &temp1_collision_val);
+	prev = map.put(&temp1, &temp1_collision_val);
 	ASSERT_EQ(1, map.current_capacity);
 	ASSERT_EQ(&temp1_val, prev);
-	prev = map.put(ptemp1, &temp1_collision_val2);
+	prev = map.put(&temp1, &temp1_collision_val2);
 	ASSERT_EQ(1, map.current_capacity);
 	ASSERT_EQ(&temp1_collision_val, prev);
-	prev = map.put(ptemp1, &temp1_collision_val3);
+	prev = map.put(&temp1, &temp1_collision_val3);
 	ASSERT_EQ(1, map.current_capacity);
 	ASSERT_EQ(&temp1_collision_val2, prev);
-	prev = map.put(ptemp1, &temp1_val);
+	prev = map.put(&temp1, &temp1_val);
 	ASSERT_EQ(1, map.current_capacity);
 	ASSERT_EQ(&temp1_collision_val3, prev);
 
 	//find_pair(found) with put test
-	pair = map.find_pair(ptemp1);
+	pair = map.find_pair(&temp1);
 	ASSERT_NOTNULL(pair);
 	ASSERT_FALSE(pair->deleted);
-	ASSERT_EQ(ptemp1, pair->key);
+	ASSERT_EQ(static_cast<const Map<int>::Hashable*>(&temp1), pair->key);
 	ASSERT_EQ(&temp1_val, pair->value);
 
 	//get
-	int* get = map.get(ptemp1);
+	int* get = map.get(&temp1);
 	ASSERT_EQ(&temp1_val, get);
+	get = map.get(&temp2);
+	ASSERT_NULL(get);
 
 	//containsKey
-	ASSERT_FALSE(map.containsKey(ptemp2));
-	ASSERT_TRUE(map.containsKey(ptemp1));
+	ASSERT_FALSE(map.containsKey(&temp2));
+	ASSERT_TRUE(map.containsKey(&temp1));
 
 	//remove
-	ASSERT_FALSE(map.remove(ptemp2));
-	ASSERT_TRUE(map.remove(ptemp1));
+	ASSERT_FALSE(map.remove(&temp2));
+	ASSERT_TRUE(map.remove(&temp1));
 
 	//get (for value deleted)
-	ASSERT_NULL(map.get(ptemp1));
+	ASSERT_NULL(map.get(&temp1));
 	
 	//collision test (50, 18, 82, 114)
 	ASSERT_EQ(1, map.current_capacity);
 	ASSERT_EQ(32, map.max_size);
-	map.put(ptemp1_collision, &temp1_collision_val);
-	map.put(ptemp1_collision2, &temp1_collision_val2);
-	map.put(ptemp1_collision3, &temp1_collision_val3);
+	map.put(&temp1_collision, &temp1_collision_val);
+	map.put(&temp1_collision2, &temp1_collision_val2);
+	map.put(&temp1_collision3, &temp1_collision_val3);
 	ASSERT_EQ(4, map.current_capacity);
 	ASSERT_EQ(32, map.max_size);
-	pair = map.find_pair(ptemp1_collision);
-	ASSERT_EQ(ptemp1_collision, pair->key);
+	pair = map.find_pair(&temp1_collision);
+	ASSERT_EQ(static_cast<const Map<int>::Hashable*>(&temp1_collision), pair->key);
 	ASSERT_EQ(&temp1_collision_val, pair->value);
-	pair = map.find_pair(ptemp1_collision2);
-	ASSERT_EQ(ptemp1_collision2, pair->key);
+	pair = map.find_pair(&temp1_collision2);
+	ASSERT_EQ(static_cast<const Map<int>::Hashable*>(&temp1_collision2), pair->key);
 	ASSERT_EQ(&temp1_collision_val2, pair->value);
-	pair = map.find_pair(ptemp1_collision3);
-	ASSERT_EQ(ptemp1_collision3, pair->key);
+	pair = map.find_pair(&temp1_collision3);
+	ASSERT_EQ(static_cast<const Map<int>::Hashable*>(&temp1_collision3), pair->key);
 	ASSERT_EQ(&temp1_collision_val3, pair->value);
 
-	ASSERT_EQ(ptemp1_collision, map.buckets[19]->key);
-	ASSERT_EQ(ptemp1_collision2, map.buckets[20]->key);
-	ASSERT_EQ(ptemp1_collision3, map.buckets[21]->key);
+	ASSERT_EQ(static_cast<const Map<int>::Hashable*>(&temp1), map.buckets[18]->key);
+	ASSERT_EQ(static_cast<const Map<int>::Hashable*>(&temp1_collision), map.buckets[19]->key);
+	ASSERT_EQ(static_cast<const Map<int>::Hashable*>(&temp1_collision2), map.buckets[20]->key);
+	ASSERT_EQ(static_cast<const Map<int>::Hashable*>(&temp1_collision3), map.buckets[21]->key);
+
+	//rehash test on put
+
+	//create new map
+	Map<int> map2;
+	//force set max_size for test
+	//this will cause memory leak as size is changed but actual size of array is same
+	map2.max_size = 3; 
+	// max_load = 3 * 0.75 = (int)2.25 = 2
+	// (18, 82, 114, 53) 18%3 = 0, 82%3 = 1, 114%3 = 0, 53%3 = 2
+	map2.put(&temp1_collision, &temp1_collision_val);
+	map2.put(&temp1_collision2, &temp1_collision_val2);
+	map2.put(&temp1_collision3, &temp1_collision_val3);
+	// the forth element should invoke rehash()
+	map2.put(&temp2, &temp2_val);
+	// check
+	ASSERT_EQ(6, map2.max_size);
+	ASSERT_EQ(4, map2.current_capacity);
 
 	///end of test
 	
