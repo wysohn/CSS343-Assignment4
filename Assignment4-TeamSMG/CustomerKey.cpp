2824f902daae386c8739d350f2ae4a353bf9e20f
Tonynology <36941592+Tonynology@users.noreply.github.com>
2019-03-18 16:16:41 -0700

    Add files via upload

---
+++ Assignment4-TeamSMG/CustomerKey.cpp
@@ -0,0 +1,25 @@
+#include "CustomerKey.h"
+
+CustomerKey::CustomerKey(int customerID, std::string firstName, std::string lastName)
+                    : customerID(customerID), firstName(firstName), lastName(lastName){
+    
+}
+
+int CustomerKey::hashCode() const{
+    return customerID + hashstring(firstName) + hashstring(lastName);
+}
+
+bool CustomerKey::equals(const Hashable* other) const{
+    if(const CustomerKey* key = dynamic_cast<const CustomerKey*>(other)){
+        if(this->customerID != key->customerID)
+            return false;
+        if(this->firstName != key->firstName)
+            return false;
+        if(this->lastName != key->lastName)
+            return falase;
+        
+        return true;
+    }else{
+        return false;
+    }
+}
