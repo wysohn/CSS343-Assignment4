7589ab1717da60a04dc008fda58149de71840b27
Tonynology <36941592+Tonynology@users.noreply.github.com>
2019-03-20 02:20:06 -0700

    Add files via upload

---
+++ Assignment4-TeamSMG/CustomerInfo.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <string>
+
+#include "map.h"
+#include "comparable.h"
+
+class CustomerInfo : public Hashable, public Comparable{
+    
+protected:
+    int customerID;
+    std::string firstName;
+    std::string lastName; 
+    
+public:
+    CustomerInfo(int customerID);
+    
+    CustomerInfo(int customerID, std::string firstName);
+    
+    CustomerInfo(int customerID, std::string firstName, std::string lastName);
+    
+        /*
+	Generate hash value based on customerID, firstName, and lastname.
+	Chidren class must override this method again to produce
+	different hash value.
+	*/
+    virtual int hashCode() const;    
+    
+	/*
+	Check if this object's customerID, firstName, and lastName
+	is exactly matching with the 'other' object's
+	customerID, firstName, and lastName. If 'other' is not
+	same type as this object, it should be always false.
+	*/
+    virtual bool equals(const Hashable* other) const;
+    
+    /*
+	All children class must implement this method
+	in order to compare depending on their cutomerID
+	Different customerID has different comparison rule.
+	*/
+    int compareTo(const Comparable* other) const;
+};
