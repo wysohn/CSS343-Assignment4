8b3c9ae0c68b0f3d4bbe12bece3a4cabfaf85d48
Jeremiah Griffith <jeremiahg101@gmail.com>
2019-03-20 04:15:45 -0700

    Redid code for sort in DatabaseMovie. Still need Write()

--- Assignment4-TeamSMG/DatabaseMovie.cpp
+++ Assignment4-TeamSMG/DatabaseMovie.cpp
@@ -6,11 +6,17 @@ void DatabaseMovie::sort()
 	std::sort(vec.begin(), vec.end(), sortHelper);
 }
 
+//Method to be passed as third parameter to overloaded std::sort() function.
+//This clarifies to the function how objects are compared, and which is
+//greater or less in value. In this case, it is in comparing two MovieKey
+//objects. The movies will be sorted by genre as follows: Comedies, Dramas,
+//and then Classics. Then each genre will be sorted based on the overloaded
+//compareTo methods in the MovieKey subclasses.
 bool DatabaseMovie::sortHelper(MovieKey *first, MovieKey *second)
 {
 
 	//MovieKeys share same subclass instance -- same genre
-	if (typeid(first) == typeid(second))
+	if (typeid(*first) == typeid(*second))
 	{
 
 		// first < second
@@ -27,10 +33,35 @@ bool DatabaseMovie::sortHelper(MovieKey *first, MovieKey *second)
 	//MovieKeys are objects of different subclasses -- different genres
 	//Compare two movies of different genres according to this order:
 	//	Comedies, Dramas, then Classics
-
 	else
 	{
+		//first is Comedy, and Comedy < [any other genre]
+		if (first->name() == "Comedy")
+		{
+			return true;
+		}
 
+		//first is Drama
+		else if (first->name() == "Drama")
+		{
+			//second is Comedy, and Drama > Comedy
+			if (second->name() == "Comedy")
+			{
+				return false;
+			}
+
+			//second is Classic, and Drama < Classic
+			else
+			{
+				return true;
+			}
+		}
+
+		//first is Classic, and Classic > [any other genre]
+		else
+		{
+			return false;
+		}
 	}
 }
 
