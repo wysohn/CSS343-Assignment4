0377029af46b4ab744d0d4123d38abb948769a92
wysohn <wysohn2002@naver.com>
2019-03-20 02:12:16 -0700

    Just some notes

--- Assignment4-TeamSMG/DatabaseMovie.h
+++ Assignment4-TeamSMG/DatabaseMovie.h
@@ -3,6 +3,13 @@
 #include "AbstractDatabase.h"
 #include <vector>
 
+/*
+Notes:
+1. The input format is always correct, but it may have invalid input
+2. Each movie has different format
+3. There can be same movies in different line (then should add up stocks)
+*/
+
 
 class DatabaseMovie : public AbstractDatabase<MovieKey, int>
 {
