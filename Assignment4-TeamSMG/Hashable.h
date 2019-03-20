f73444a3b4d0e48e653147ba7394a0db643e594d
wysohn <wysohn2002@naver.com>
2019-03-19 17:57:45 -0700

    Fixed unsigned integer conversion warning

--- Assignment4-TeamSMG/Hashable.h
+++ Assignment4-TeamSMG/Hashable.h
@@ -17,7 +17,7 @@ public:
 	*/
 	static int hashString(std::string str) {
 		int hash = 0;
-		for (int i = 0; i < str.length(); i++) {
+		for (size_t i = 0; i < str.length(); i++) {
 			hash += str[i];
 		}
 		return 31 * hash;
